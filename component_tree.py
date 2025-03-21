40f994b7eeac29b76aef7797ca50de8e6b9ed3db
Romain PERRIN <romain.perrin@unistra.fr>
2025-03-21 15:54:29 +0100

    refactoring component_tree.py

--- component_tree.py
+++ component_tree.py
@@ -11,209 +11,303 @@ import pydot
 #------------------------------------------------------------------------------
 
 class ComponentTree():
-    """
-    Represents a component-tree, with a root node and a list of nodes (ComponentTreeNode).
+	"""
+	Represents a component-tree, with a root node and a list of nodes (ComponentTreeNode).
 
-    ...
+	Attributes
+	----------
+	root : int
+		The index of the root node.
+	nodes : list[ComponentTreeNode]
+		A list of nodes (*ComponentTreeNode*).
+	invert : bool
+		Whether the image has to be inverted (default is bright objects over a dark background).
 
-    Attributes
-    ----------
-    root : int
-        index of the root node
-    nodes : list
-        list of nodes (ComponentTreeNode)
-    invert : bool
-        whether the image has to be inverted (default if bright objects on dark background)
+	Methods
+	-------
+	print_tree(node):
+		Prints the tree structure in the console starting at the given node.
+	save_dot(filename):
+		Writes the component-tree in the DOT language into a dot file.
+	build_component_tree(image, invert):
+		Builds the component-tree of the given image using Najman's algorithm.
+	build_component_tree_from_partial_image(image, mask, invert):
+		Builds the component-tree of a partial image using Najman's algorithm.
+	reconstruct_image(image):
+		Reconstructs an image using the its component-tree.
 
-    Methods
-    -------
-    print_tree(node):
-        print the tree structure in the console starting at the given node
-    save_dot(filename):
-        writes the component-tree in the DOT language into a dot file
-    build_component_tree(image, invert):
-        builds the component-tree of the given image using Najman's algorithm
-    build_component_tree_from_partial_image(image, mask, invert):
-        builds the component-tree of a partial image using Najman's algorithm
-    reconstruct_image(image):
-        reconstructs an image using the component-tree
-    """
+	Written by Romain PERRIN (<romain.perrin@unistra.fr>).
+	"""
 
-    def __init__(self):
-        '''
-        Constructor - initializes the root to 0, creates an empty list of nodes and sets invert to False.
-        '''
-        self.root = 0 # index of the root
-        self.nodes = list()  # list of nodes
-        self.invert = False
+	def __init__(self):
+		"""
+		Constructor - initializes the root to 0, creates an empty list of nodes and sets invert to *False*.
 
-    #--------------------------------------------------------------------------
+		Written by Romain PERRIN (<romain.perrin@unistra.fr>).
+		"""
 
-    def get_root(self) -> int:
-        '''
-        Getter for attribute `root`
-        '''
-        return self.root
+		self.root = 0 # index of the root
+		self.nodes = list()  # list of nodes
+		self.invert = False
 
-    def set_root(self, root: int):
-        '''
-        Setter for attribute `root`
-        '''
-        self.root = root
+	#--------------------------------------------------------------------------
 
-    #----------------------------------------
+	def get_root(self) -> int:
+		"""
+		Getter for attribute **root**.
 
-    def get_nodes(self) -> list:
-        '''
-        Getter for attribute `nodes`
-        '''
-        return self.nodes
+		Returns
+		-------
+		: int
+			The ID of the component tree's root node.
 
-    def set_nodes(self, nodes: list) -> None:
-        '''
-        Setter for attribute `nodes`
-        '''
-        self.nodes = nodes
+		Written by Romain PERRIN (<romain.perrin@unistra.fr>).
+		"""
 
-    #----------------------------------------
+		return self.root
+	
+	#--------------------------------------------------------------------------
 
-    def get_invert(self) -> bool:
-        '''
-        Getter for attribute `invert`
-        '''
-        return self.invert
+	def set_root(self, root: int):
+		"""
+		Setter for attribute **root**.
 
-    def set_invert(self, invert: bool) -> None:
-        '''
-        Setter for attribute `invert`
-        '''
-        self.invert = invert
+		Parameters
+		----------
+		root : int
+			The index of the component tree's root node.
 
-    #--------------------------------------------------------------------------
+		Written by Romain PERRIN (<romain.perrin@unistra.fr>).
+		"""
 
-    def print_tree(self, node: int) -> None:
-        '''
-        Displays the global structure of the component-tree starting at a given node.
+		self.root = root
 
-                Parameters:
-                        `node` (int): Index of the root node
-        '''
-        print(f"# Node ({node}) :")
-        print(f"    - Level   -> {self.get_nodes()[node].get_level()}")
-        print(f"    - Highest   -> {self.get_nodes()[node].get_highest()}")
-        print(f"    - Area   -> {self.get_nodes()[node].get_area()}")
-        print(f"    - Father   -> {self.get_nodes()[node].get_father()}")
-        print(f"    - Children   -> [")
-        for child in self.get_nodes()[node].get_children():
-            print(f"{child}, ")
-        print(f"]")
+	#--------------------------------------------------------------------------
 
-        for child in self.get_nodes()[node].get_children():
-            self.print_tree(child)
+	def get_nodes(self) -> list:
+		"""
+		Getter for attribute **nodes**.
 
-    #--------------------------------------------------------------------------
+		Returns
+		-------
+		: list[ComponentTreeNode]
+			The list of nodes (including the root), objects are instances of ComponentTreeNode.
 
-    def save_dot(self, filename: str) -> None:
-        '''
-        Saves the current component-tree to a dot file using the DOT language and pyDot/GraphViz.
+		Written by Romain PERRIN (<romain.perrin@unistra.fr>).
+		"""
 
-                Parameters:
-                        `filename` (str): path to the output file without extension
+		return self.nodes
+	
+	#--------------------------------------------------------------------------
 
-                Returns:
-                        None
-        '''
+	def set_nodes(self, nodes: list) -> None:
+		"""
+		Setter for attribute **nodes**.
 
-        graph = pydot.Dot('component_tree', graph_type='graph', bgcolor='white')
+		Parameters
+		----------
+		nodes : list[ComponentTreeNode]
+			The list of nodes as a list of ComponentTreeNode objects.
 
-        # add all nodes from root to leaves
-        for node in self.get_nodes():
-            node = pydot.Node(name=f"node_{node.get_id()}")
-            graph.add_node(node)
+		Written by Romain PERRIN (<romain.perrin@unistra.fr>).
+		"""
 
-        # connect nodes
-        process = set()
-        process.add(self.get_nodes()[self.get_root()])
+		self.nodes = nodes
 
-        while len(process) > 0:
-            cur_node = process.pop()
-            for child_index in cur_node.get_children():
-                child_node = self.get_nodes()[child_index]
-                process.add(child_node)
-                edge = pydot.Edge(f"node_{cur_node.get_id()}", f"node_{child_node.get_id()}")
-                graph.add_edge(edge)
+	#--------------------------------------------------------------------------
 
-        #graph.write_dot(f"{filename}.dot")
-        graph.write_png(f"{filename}.png")
+	def get_invert(self) -> bool:
+		"""
+		Getter for attribute **invert**.
 
-    #--------------------------------------------------------------------------
+		Returns
+		-------
+		: bool
+			*True* if the component-tree is built for bright objects over a dark background, *False* otherwise.
 
-    def build_component_tree(self, image: np.ndarray, invert_image: bool) -> None:
-        '''
-        Builds a component-tree using an implementation based on Najman's algorithm published in :
-        L.Najman, M.Croupie, "Building the component-tree in quasi-linear time", Vol. 15, Num. 11, p. 3531-3539, 2006
+		Written by Romain PERRIN (<romain.perrin@unistra.fr>).
+		"""
 
-                Parameters:
-                        `image` (nparray): Numpy array of the image
-                        `invert_image` (bool): whether the input image should be inverted
+		return self.invert
+	
+	#--------------------------------------------------------------------------
 
-                Returns:
-                        None
-        '''
-        # placeholder, implementation in derived classes
-        pass
+	def set_invert(self, invert: bool) -> None:
+		"""
+		Setter for attribute **invert**.
 
-    #--------------------------------------------------------------------------
+		Parameters
+		----------
+		invert: bool
+			*True* is the component-tree is built for bright objects over a dark background, *False* otherwise.
 
-    def build_component_tree_from_partial_image(self, image: np.ndarray, mask:np.ndarray, invert_image: bool) -> None:
-        '''
-        Builds a component-tree on a given set of pixels using an implementation based on Najman's algorithm published in :
-        L.Najman, M.Croupie, "Building the component-tree in quasi-linear time", Vol. 15, Num. 11, p. 3531-3539, 2006
+		Written by Romain PERRIN (<romain.perrin@unistra.fr>).
+		"""
 
-                Parameters:
-                        `image` (nparray): Numpy array of the image
-                        `mask` (ndarray): Numpy array of the same size as image, indicating which pixels should be processed
-                        `invert_image` (bool): whether the input image should be inverted
+		self.invert = invert
 
-                Returns:
-                        None
-        '''
-        # placeholder, implementation in derived classes
-        pass
+	#--------------------------------------------------------------------------
 
-    #--------------------------------------------------------------------------
+	def print_tree(self, node: int) -> None:
+		"""
+		Displays the global structure of the component-tree starting at a given node.
 
-    def reconstruct_image(self, image: np.ndarray) -> np.ndarray:
-        '''
-        Reconstructs the image using the component-tree.
+		Parameters
+		----------
+		node: int
+			The index of the root node (can be any node in the component-tree).
 
-                Parameters:
-                        `image` (ndarray): Numpy array of the original image
+		Returns
+		-------
+		Prints on the standard output.
 
-                Returns:
-                        `reconstructed` (ndarray): Numpy array of the reconstructed image
-        '''
-        reconstructed = np.zeros(image.shape, dtype=np.uint8)
-        reconstructed = reconstructed.flatten()
+		Written by Romain PERRIN (<romain.perrin@unistra.fr>).
+		"""
 
-        to_process = []
-        root_node = self.nodes()[self.root()]
-        for pixel in root_node.get_pixels():
-            reconstructed[pixel] = root_node.get_level()
-        for child_index in root_node.get_children():
-            to_process.append(child_index)
+		print(f"# Node ({node}) :")
+		print(f"    - Level   -> {self.get_nodes()[node].get_level()}")
+		print(f"    - Highest   -> {self.get_nodes()[node].get_highest()}")
+		print(f"    - Area   -> {self.get_nodes()[node].get_area()}")
+		print(f"    - Father   -> {self.get_nodes()[node].get_father()}")
+		print(f"    - Children   -> [")
+		for child in self.get_nodes()[node].get_children():
+			print(f"{child}, ")
+		print(f"]")
 
-        while len(to_process) > 0:
-            node_index = to_process.pop(0)
-            node = self.nodes()[node_index]
-            for pixel in node.get_pixels():
-                reconstructed[pixel] = node.get_level()
-            for child_index in node.get_children():
-                to_process.append(child_index)
+		for child in self.get_nodes()[node].get_children():
+			self.print_tree(child)
 
-        reconstructed = reconstructed.reshape(image.shape)
+	#--------------------------------------------------------------------------
 
-        if self.get_invert():
-            reconstructed = 255 - reconstructed
+	def save_dot(self, filename: str) -> None:
+		"""
+		Saves the current component-tree to a dot file using the DOT language and pyDot/GraphViz.
 
-        return reconstructed
+		Parameters
+		----------
+		filename: str
+			The path to the output file without the extension.
+
+		Returns
+		-------
+		None
+
+		Written by Romain PERRIN (<romain.perrin@unistra.fr>).
+		"""
+
+		graph = pydot.Dot('component_tree', graph_type='graph', bgcolor='white')
+
+		# add all nodes from root to leaves
+		for node in self.get_nodes():
+			node = pydot.Node(name=f"node_{node.get_id()}")
+			graph.add_node(node)
+
+		# connect nodes
+		process = set()
+		process.add(self.get_nodes()[self.get_root()])
+
+		while len(process) > 0:
+			cur_node = process.pop()
+			for child_index in cur_node.get_children():
+				child_node = self.get_nodes()[child_index]
+				process.add(child_node)
+				edge = pydot.Edge(f"node_{cur_node.get_id()}", f"node_{child_node.get_id()}")
+				graph.add_edge(edge)
+
+		#graph.write_dot(f"{filename}.dot")
+		graph.write_png(f"{filename}.png")
+
+	#--------------------------------------------------------------------------
+
+	def build_component_tree(self, image: np.ndarray, invert_image: bool) -> None:
+		"""
+		Builds a component-tree using an implementation based on Najman's algorithm published in :
+		L.Najman, M.Croupie, "Building the component-tree in quasi-linear time", Vol. 15, Num. 11, p. 3531-3539, 2006.
+
+		Parameters
+		----------
+		image: ndarray
+			The grayscale image on which to build the component-tree (as a Numpy array).
+		invert_image: bool
+			Whether the input image should be inverted (use *True* for bright objects on a dark background, *False* for the opposite).
+
+		Returns
+		-------
+		None
+
+		Written by Romain PERRIN (<romain.perrin@unistra.fr>).
+		"""
+
+		# placeholder, implementation in derived classes
+		pass
+
+	#--------------------------------------------------------------------------
+
+	def build_component_tree_from_partial_image(self, image: np.ndarray, mask:np.ndarray, invert_image: bool) -> None:
+		"""
+		Builds a component-tree on a given set of pixels using an implementation based on Najman's algorithm published in :
+		L.Najman, M.Croupie, "Building the component-tree in quasi-linear time", Vol. 15, Num. 11, p. 3531-3539, 2006.
+		Same principle as *build_component_tree()* but on a given subset of pixels of the input image.
+
+		Parameters
+		----------
+		image: ndarray:
+			The grayscale image on which to build the component-tree (as aNumpy array).
+		mask: ndarray
+			A mask image of the same size as **image** indicating which pixels should be processed (Numpy array).
+		invert_image: bool
+			Whether the input image should be inverted (use *True* for bright objects on a dark background, *False* for the opposite).
+
+		Returns
+		-------
+		None
+
+		Written by Romain PERRIN (<romain.perrin@unistra.fr>).
+		"""
+
+		# placeholder, implementation in derived classes
+		pass
+
+	#--------------------------------------------------------------------------
+
+	def reconstruct_image(self, image: np.ndarray) -> np.ndarray:
+		"""
+		Reconstructs the image using its component-tree.
+
+		Parameters
+		----------
+		image: ndarray
+			The Numpy array of the original image (used to set the resulting image' shape and type)
+
+		Returns
+		-------
+		reconstructed: ndarray
+			The grayscale reconstructed image from the stored component-tree as a Numpy array (same shape and type as **image**).
+
+		Written by Romain PERRIN (<romain.perrin@unistra.fr>).
+		"""
+
+		reconstructed = np.zeros(image.shape, dtype=np.uint8)
+		reconstructed = reconstructed.flatten()
+
+		to_process = []
+		root_node = self.nodes()[self.root()]
+		for pixel in root_node.get_pixels():
+			reconstructed[pixel] = root_node.get_level()
+		for child_index in root_node.get_children():
+			to_process.append(child_index)
+
+		while len(to_process) > 0:
+			node_index = to_process.pop(0)
+			node = self.nodes()[node_index]
+			for pixel in node.get_pixels():
+				reconstructed[pixel] = node.get_level()
+			for child_index in node.get_children():
+				to_process.append(child_index)
+
+		reconstructed = reconstructed.reshape(image.shape)
+
+		if self.get_invert():
+			reconstructed = 255 - reconstructed
+
+		return reconstructed
--- component_tree_utils.py
+++ component_tree_utils.py
@@ -14,146 +14,146 @@ from tarjan_union_find import TarjanUnionFind
 #------------------------------------------------------------------------------
 
 def make_node(point: ComponentTreePoint) -> ComponentTreeNode:
-    """
-    Creates a node the size of a point.
+	"""
+	Creates a node the size of a point.
 
-    Parameters
-    ----------
-    point: ComponentTreePoint
-        A point (i.e. a pixel).
+	Parameters
+	----------
+	point: ComponentTreePoint
+		A point (i.e. a pixel).
 
-    Returns
-    -------
-    n: ComponentTreeNode
-        A new node containing the given point.
+	Returns
+	-------
+	n: ComponentTreeNode
+		A new node containing the given point.
 
-    Written by Romain PERRIN (<romain.perrin@unistra.fr>).
-    """
+	Written by Romain PERRIN (<romain.perrin@unistra.fr>).
+	"""
 
-    n = ComponentTreeNode()
-    n.set_index(point.get_index())
-    n.set_level(point.get_value())
-    n.set_highest(point.get_value())
-    n.set_area(1)
-    n.set_subarea(1)
-    n.set_father(point.get_index())
-    n.set_children(set())
-    n.get_pixels().add(point.get_index())
-    return n
+	n = ComponentTreeNode()
+	n.set_index(point.get_index())
+	n.set_level(point.get_value())
+	n.set_highest(point.get_value())
+	n.set_area(1)
+	n.set_subarea(1)
+	n.set_father(point.get_index())
+	n.set_children(set())
+	n.get_pixels().add(point.get_index())
+	return n
 
 #------------------------------------------------------------------------------
 
 def merge_nodes(nodes: list, q: ComponentTreeSet, n1: int, n2: int) -> int:
-    """
-    Merge two nodes (attributes, etc.) together and return the index of the resulting node.
+	"""
+	Merge two nodes (attributes, etc.) together and return the index of the resulting node.
 
-    Parameters
-    ----------
-    nodes: ComponentTreeNode[]
-        The list of nodes.
-    q: ComponentTreeSet)
-        The collection Q.
-    n1: int
-        Index of the first node.
-    n2: int
-        Index of the second node.
+	Parameters
+	----------
+	nodes: ComponentTreeNode[]
+		The list of nodes.
+	q: ComponentTreeSet)
+		The collection Q.
+	n1: int
+		Index of the first node.
+	n2: int
+		Index of the second node.
 
-    Returns
-    -------
-    tmp_n1: int
-        The index of the resulting node.
+	Returns
+	-------
+	tmp_n1: int
+		The index of the resulting node.
 
-    Written by Romain PERRIN (<romain.perrin@unistra.fr>).
-    """
+	Written by Romain PERRIN (<romain.perrin@unistra.fr>).
+	"""
 
-    tmp_n1 = TarjanUnionFind.link(q, n1, n2)
-    if tmp_n1 == n2:
-        tmp_n2 = n1
-    else:
-        tmp_n2 = n2
+	tmp_n1 = TarjanUnionFind.link(q, n1, n2)
+	if tmp_n1 == n2:
+		tmp_n2 = n1
+	else:
+		tmp_n2 = n2
 
-    # update attributes
-    nodes[tmp_n1].set_highest(max(nodes[tmp_n1].get_highest(), nodes[tmp_n2].get_highest()))
-    nodes[tmp_n1].set_area(nodes[tmp_n1].get_area() + nodes[tmp_n2].get_area())
-    nodes[tmp_n1].get_pixels().update(nodes[tmp_n2].get_pixels())
-    nodes[tmp_n2].get_pixels().clear()
-    nodes[tmp_n1].set_subarea(nodes[tmp_n1].get_subarea() + nodes[tmp_n2].get_subarea())
+	# update attributes
+	nodes[tmp_n1].set_highest(max(nodes[tmp_n1].get_highest(), nodes[tmp_n2].get_highest()))
+	nodes[tmp_n1].set_area(nodes[tmp_n1].get_area() + nodes[tmp_n2].get_area())
+	nodes[tmp_n1].get_pixels().update(nodes[tmp_n2].get_pixels())
+	nodes[tmp_n2].get_pixels().clear()
+	nodes[tmp_n1].set_subarea(nodes[tmp_n1].get_subarea() + nodes[tmp_n2].get_subarea())
 
-    # add the list of children of the node that is not kept to the list of children of the node that is kept
-    for child in nodes[tmp_n2].get_children():
-        nodes[tmp_n1].get_children().add(child)
-        nodes[child].set_father(tmp_n1)
+	# add the list of children of the node that is not kept to the list of children of the node that is kept
+	for child in nodes[tmp_n2].get_children():
+		nodes[tmp_n1].get_children().add(child)
+		nodes[child].set_father(tmp_n1)
 
-    nodes[tmp_n2].set_children([])
-    return tmp_n1
+	nodes[tmp_n2].set_children([])
+	return tmp_n1
 
 #------------------------------------------------------------------------------
 
 def get_neighbours(x: int, ny: int, nx: int) -> list:
-    """
-    Retrieves the 4-neighbourdhood of the point of index **x**.
+	"""
+	Retrieves the 4-neighbourdhood of the point of index **x**.
 
-    Parameters
-    ----------
-    x: int
-        The index of the point.
-    ny: int
-        Number of rows.
-    nx: int
-        Number of columns.
+	Parameters
+	----------
+	x: int
+		The index of the point.
+	ny: int
+		Number of rows.
+	nx: int
+		Number of columns.
 
-    Returns
-    -------
-    neighbours: int[]
-        The list of indices representing the neighbouring points of **x**.
+	Returns
+	-------
+	neighbours: int[]
+		The list of indices representing the neighbouring points of **x**.
 
-    Written by Romain PERRIN (<romain.perrin@unistra.fr>).
-    """
+	Written by Romain PERRIN (<romain.perrin@unistra.fr>).
+	"""
 
-    neighbours = []
+	neighbours = []
 
-    # if the point is not on the very top of the image
-    if math.floor(x / nx) > 0:
-        neighbours.append(x - nx)
+	# if the point is not on the very top of the image
+	if math.floor(x / nx) > 0:
+		neighbours.append(x - nx)
 
-    # if the point is not on the very left of the image
-    if x % nx > 0:
-        neighbours.append(x - 1)
+	# if the point is not on the very left of the image
+	if x % nx > 0:
+		neighbours.append(x - 1)
 
-    # if the point is not on the very right of the image
-    if x % nx < nx - 1:
-        neighbours.append(x + 1)
+	# if the point is not on the very right of the image
+	if x % nx < nx - 1:
+		neighbours.append(x + 1)
 
-    # if the point is not on the very bottom of the image
-    if math.floor(x / nx) < ny - 1:
-        neighbours.append(x + nx)
+	# if the point is not on the very bottom of the image
+	if math.floor(x / nx) < ny - 1:
+		neighbours.append(x + nx)
 
-    return neighbours
+	return neighbours
 
 #------------------------------------------------------------------------------
 
 def lex_sort(a: ComponentTreePoint, b: ComponentTreePoint) -> bool:
-    """
-    Lexicographical order comparison in decreasing order of level between points.
-    For example : (12, 120) < (3, 50) < (4, 50) < (6, 40)
+	"""
+	Lexicographical order comparison in decreasing order of level between points.
+	For example : (12, 120) < (3, 50) < (4, 50) < (6, 40)
 
-    Parameters
-    ----------
-    a: ComponentTreePoint
-        The point **A**.
-    b: ComponentTreePoint
-        The point **B**.
+	Parameters
+	----------
+	a: ComponentTreePoint
+		The point **A**.
+	b: ComponentTreePoint
+		The point **B**.
 
-    Returns
-    -------
-    lex_sort: bool
-        *True* if **A** > **B** in the decresing lexicographical order, *False* otherwise.
+	Returns
+	-------
+	lex_sort: bool
+		*True* if **A** > **B** in the decresing lexicographical order, *False* otherwise.
 
-    Written by Romain PERRIN (<romain.perrin@unistra.fr>).
-    """
+	Written by Romain PERRIN (<romain.perrin@unistra.fr>).
+	"""
 
-    if a.value() > b.value():
-        return True
-    if b.value() > a.value():
-        return False
-    return a.index() < b.index()
+	if a.value() > b.value():
+		return True
+	if b.value() > a.value():
+		return False
+	return a.index() < b.index()
