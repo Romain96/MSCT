5de683ffc17caa4bf33c0a90d14bf0fa71b34a0e
Romain PERRIN <romain.perrin@unistra.fr>
2025-03-25 14:53:03 +0100

    refactoring index_to_pixel.py

--- index_to_pixel.py
+++ index_to_pixel.py
@@ -9,93 +9,149 @@ __copyright__ = "Copyright 2023, Romain PERRIN, SDC Team, ICube UMR 7357, Univer
 #------------------------------------------------------------------------------
 
 class Index2Pixel():
-    """
-    Utility class providing methods to convert indices to pixels and vice versa
-    """
+	"""
+	This utility class provides methods to convert 1D integer indices to 2D pixels and vice versa.
 
-    def __init__(self, width: int):
-        '''
-        Constructor - sets width to the user-defined width
-        '''
-        self.width = width
+	Written by Romain PERRIN (<romain.perrin@unistra.fr>).
+	"""
 
-    #--------------------------------------------------------------------------
+	def __init__(self, width: int):
+		"""
+		Constructor - sets **width** to the user-defined width.
 
-    def get_width(self) -> int:
-        '''
-        Getter for attribute `width`
-        '''
-        return self.width
+		Parameters
+		----------
+		width: int
+			Width of the image serving as a base for indexing the pixels.
 
-    def set_width(self, width: int) -> None:
-        '''
-        Setter for attribute `width`
-        '''
-        self.width = width
+		Written by Romain PERRIN (<romain.perrin@unistra.fr>).
+		"""
 
-    #--------------------------------------------------------------------------
+		self.width = width
 
-    def convert_pixel_to_index(self, pixel: tuple) -> int:
-        '''
-        Converts a pixel represented by a tuple to an index
+	#--------------------------------------------------------------------------
 
-                Parameters:
-                        `pixel` (tuple): (row, col) pixel
+	def get_width(self) -> int:
+		"""
+		Getter for attribute **width**.
 
-                Returns:
-                        `index` (int): index pixel
-        '''
-        row, col = pixel
-        index = row * self.get_width() + col
-        return index
-    
-    #--------------------------------------------------------------------------
+		Returns
+		-------
+		: int
+			Width of the image serving as a base for indexing the pixels.
 
-    def convert_pixels_to_indices(self, pixels: set) -> set:
-        '''
-        Converts a set of pixels represented by tuples to a set of indices
+		Written by Romain PERRIN (<romain.perrin@unistra.fr>).
+		"""
 
-                Parameters:
-                        `pixels` (set): set of (row, col) pixels
+		return self.width
+	
+	#--------------------------------------------------------------------------
 
-                Returns:
-                        `indices` (set): set of indices of pixels
-        '''
-        indices = set()
-        for pixel in pixels:
-            indices.add(self.convert_pixel_to_index(pixel))
-        return indices
+	def set_width(self, width: int) -> None:
+		"""
+		Setter for attribute **width**.
 
-    #--------------------------------------------------------------------------
+		Parameters
+		----------
+		width: int
+			Width of the image serving as a base for indexing the pixels.
 
-    def convert_index_to_pixel(self, index: int) -> tuple:
-        '''
-        Converts a pixel represented by an index to a tuple
+		Written by Romain PERRIN (<romain.perrin@unistra.fr>).
+		"""
 
-                Parameters:
-                        `index` (int): index of pixel
+		self.width = width
 
-                Returns:
-                        (tuple): (row, col) pixel
-        '''
-        row = index // self.get_width()
-        column = index % self.get_width()
-        return (row, column)
+	#--------------------------------------------------------------------------
 
-    #--------------------------------------------------------------------------
+	def convert_pixel_to_index(self, pixel: tuple[int, int]) -> int:
+		"""
+		Converts a pixel represented by a tuple to an index
 
-    def convert_indices_to_pixels(self, indices: set) -> set:
-        '''
-        Converts all pixels represented by a set of indices to a set of tuples
+		Parameters
+		----------
+		pixel: tuple[int, int]
+			2D integer pixel designated by its (row, column) coordinate. 
 
-                Parameters:
-                        `indices` (set): set of indices of pixels
+		Returns
+		-------
+		index: int
+			1D index of the input 2D **pixel** in the current referential.
 
-                Returns:
-                        `all_pixels` (set): set of (row, col) pixels
-        '''
-        all_pixels = set()
-        for index in indices:
-            pixel = self.convert_index_to_pixel(index)
-            all_pixels.add(pixel)
-        return all_pixels
+		Written by Romain PERRIN (<romain.perrin@unistra.fr>).
+		"""
+
+		row, col = pixel
+		index = row * self.get_width() + col
+		return index
+	
+	#--------------------------------------------------------------------------
+
+	def convert_pixels_to_indices(self, pixels: set[tuple[int, int]]) -> set[int]:
+		"""
+		Converts a set of 2D pixels to a set of 1D pixel indices in the current referential.
+
+		Parameters
+		----------
+		pixels: set[tuple[int, int]]
+			Set of 2D pixels represented by their (row, column) coordinates.
+
+		Returns
+		-------
+		indices: set[int]
+			Set of 1D pixel indices representing the 2D indices in the current referential.
+
+		Written by Romain PERRIN (<romain.perrin@unistra.fr>).
+		"""
+
+		indices = set()
+		for pixel in pixels:
+			indices.add(self.convert_pixel_to_index(pixel))
+		return indices
+
+	#--------------------------------------------------------------------------
+
+	def convert_index_to_pixel(self, index: int) -> tuple[int, int]:
+		"""
+		Converts a pixel represented by a 1D index to a tuple of 'row, column) in the current referential.
+
+		Parameters
+		----------
+		index: int
+			1D index of the pixel in the current referential.
+
+		Returns
+		-------
+		tuple[int, int]
+			2D pixel represented by its (row, column) coordinate from the current referential.
+
+		Written by Romain PERRIN (<romain.perrin@unistra.fr>).
+		"""
+
+		row = index // self.get_width()
+		column = index % self.get_width()
+		return (row, column)
+
+	#--------------------------------------------------------------------------
+
+	def convert_indices_to_pixels(self, indices: set[int]) -> set[tuple[int, int]]:
+		"""
+		Converts all 1D pixel indices in the current referential to a set of 2D (row, column) pixels.
+
+		Parameters
+		----------
+		indices: set[int]
+			Set of 1D pixel indices in the current referential.
+
+		Returns
+		-------
+		all_pixels: set[tuple[int, int]]
+			Set of 2D pixels represented by their (row, column) coordiantes from the current referential.
+
+		Written by Romain PERRIN (<romain.perrin@unistra.fr>).
+		"""
+
+		all_pixels = set()
+		for index in indices:
+			pixel = self.convert_index_to_pixel(index)
+			all_pixels.add(pixel)
+		return all_pixels
